43807d7089d21f4ced2b86bba32f3e2767011953
reza gholamalitabar <reza.gh.fi@gmail.com>
2022-12-14 09:18:46 +0330

    create home app

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-ju-f=b)9ur%6$yeggyud+oev3f%&0+1mk-udwe%1-@xm-12((p
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
