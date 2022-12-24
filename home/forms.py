a43d0dcc570261a3cac8d3094f0475659a7a3dc9
reza gholamalitabar <reza.gh.fi@gmail.com>
2022-12-24 12:30:23 +0330

    fix rtl html css

--- home/forms.py
+++ home/forms.py
@@ -1,13 +1,18 @@
 from django import forms
 from .models import Todo
 class TodoCreateForm(forms.Form):
-    title = forms.CharField(max_length=100, label='onvam')
-    body = forms.CharField()
-    created = forms.DateTimeField()
+    title = forms.CharField(max_length=100, label='عنوان کار:')
+    body = forms.CharField(label="متن کار:")
+    created = forms.DateTimeField(label="تاریخ ایجاد:")
 
 class TodoUpdateForm(forms.ModelForm):
     class Meta:
         model = Todo
         # fields = ['title', 'body', 'created']
+        labels = {
+            "title": "عنوان کار::",
+            "body":"متن کار:",
+            "created":"تاریخ ایجاد:"
+        }
         fields = '__all__'
 
